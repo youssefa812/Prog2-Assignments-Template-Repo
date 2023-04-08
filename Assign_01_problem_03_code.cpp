59921a72284df0548f866556369ce2b77ca3903d
youssefa812 <125455227+youssefa812@users.noreply.github.com>
2023-04-08 02:40:36 +0200

    Add files via upload

---
+++ Assign_01_problem_03_code.cpp
@@ -0,0 +1,15 @@
+//youssef ahmed hassan 
+//202106018
+
+
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+int main()
+{
+ 
+	int x; long long y; char z; float g;double v;
+	cin >> x >> y >> z >> g>>v;
+	cout << x << endl << y << endl << z << endl << g << endl<<v<<endl;
+}
---
+++ Assign_01_problem_05_code.cpp
@@ -0,0 +1,14 @@
+//youssef ahmed hassan 
+//202106018
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	long long n1, n2;
+	cin >> n1 >> n2;
+	cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+	cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+	cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+	return 0;
+}
