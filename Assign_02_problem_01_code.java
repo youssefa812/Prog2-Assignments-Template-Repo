496e7aca3ff0978c89121fb97dcfc4c361a4820f
youssefa812 <125455227+youssefa812@users.noreply.github.com>
2023-04-27 15:44:15 +0200

    Add files via upload

---
+++ Assign_02_problem_01_code.java
@@ -0,0 +1,12 @@
+//youssef ahmed hassan
+//202106018
+import java.util.Scanner;
+public class assignment2_1 {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        int N =SC.nextInt();
+        for(int i = 1 ; i <= N ; i++){
+            System.out.println(i);
+        }
+    }
+}
